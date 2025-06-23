8f1ec9956ed1c5c01a3fecd425aa860c2681aa28
Raul Ibarra <raul.ibarra004@gmail.com>
2025-06-23 08:54:03 -0700

    feat(certificates): Agregar funcionalidad para mostrar certificados
    Se implemento la lógica para mostrar la lista de certificacos obtenidos por el usuario, dichos certificados son importados de un json el cual contiene un arreglo con dichos datos.

---
+++ src/app/core/models/certificates.model.ts
@@ -0,0 +1,5 @@
+export interface CertificateList {
+  nombre: string;
+  plataforma: string;
+  url: string;
+}
--- src/app/features/certificates/certificates.component.ts
+++ src/app/features/certificates/certificates.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import certData from '../../../assets/data/certificates.json';
+import { CertificateList } from '../../core/models/certificates.model';
 
 @Component({
   selector: 'app-certificates',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './certificates.component.html',
-  styleUrl: './certificates.component.scss'
+  styleUrls: ['./certificates.component.scss'],
 })
 export class CertificatesComponent {
+  //Se obtiene el array de certificados
+  certificatesList: CertificateList[] = certData;
 
+  constructor() {}
 }
